d1db4a8fcfbfb0014ae871a52e8be192796688e0
Therese Latorre <tlatorre@ssct.edu.ph>
2026-05-08 15:24:38 +0800

    update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -18,21 +18,22 @@ class Item extends Model
         'status',
         'reporter_name',
         'contact_no',
+        'photo',  
     ];
 
-    // Item belongs to a User
+    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Item has matches as a Lost item ✅ added
+
     public function lostMatches()
     {
         return $this->hasMany(ItemMatch::class, 'lost_item_id');
     }
 
-    // Item has matches as a Found item ✅ added
+    
     public function foundMatches()
     {
         return $this->hasMany(ItemMatch::class, 'found_item_id');
